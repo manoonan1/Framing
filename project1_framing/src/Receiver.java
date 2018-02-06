d4c07f14c5858abb4df7d5a0b95dfbba35abc92a
manoonan1 <mnoonan136@gmail.com>
2018-02-05 20:14:27 -0500

    Add files via upload

---
+++ project1_framing/src/Receiver.java
@@ -0,0 +1,18 @@
+public class Receiver {
+
+    public static void main (String[] args){
+
+    }
+
+    public static String bitStuffing(){
+        return "";
+    }
+
+    public static String byteStuffing(){
+        return "";
+    }
+
+    public static String byteCount(){
+        return "";
+    }
+}
---
+++ project1_framing/src/Sender.java
@@ -0,0 +1,19 @@
+public class Sender {
+
+    public static void main (String[] args){
+        
+    }
+
+    public static String bitStuffing(){
+        return "";
+    }
+
+    public static String byteStuffing(){
+        return "";
+    }
+
+    public static String byteCount(){
+        return "";
+    }
+
+}
