7dd42737bd16a416dfb1cdc36490b6274e19ee7d
Michael Noonan <manoonan@bsu.edu>
2018-02-07 13:14:41 -0500

    need to fix error messages on byteCount and byteStuffing, no byteStuffing on Sender side

--- project1_framing/src/Receiver.java
+++ project1_framing/src/Receiver.java
@@ -1,18 +1,56 @@
+import java.util.Scanner;
+
 public class Receiver {
 
     public static void main (String[] args){
+        System.out.println("Enter which framing method you would like to use: ");
+        System.out.println("Choose: Bit Stuffing, Byte Stuffing, or Byte Count");
+//        byteCount();
+//        byteStuffing();
+        bitStuffing();
 
     }
 
-    public static String bitStuffing(){
-        return "";
+    public static void bitStuffing(){
+        System.out.println("Enter a message to the receiver in the format of a valid sender: ");
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        String flag = "111111";
+        int oneCount = 0;
+        message = message.replaceAll(flag, "");
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) == '1'){
+                oneCount++;
+                if(oneCount == 6){
+                    message = message.substring(0, i) + "" + message.substring(i, message.length());
+                    oneCount = 0;
+                }
+            }
+        }
+        System.out.println(message);
     }
 
-    public static String byteStuffing(){
-        return "";
+    public static void byteStuffing(){
+        System.out.println("Enter a message to the receiver in the format of a valid sender: ");
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        message = message.replaceAll("[EFef]", "");
+//        if (message.contains("E") || message.contains("F")){
+//            System.out.println("The received frame has an error!");
+//            return;
+//        }
+        System.out.println(message);
     }
 
-    public static String byteCount(){
-        return "";
+    public static void byteCount(){
+        System.out.println("Enter a message to the receiver in the format of a valid sender: ");
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        int byteNumber = (int) message.charAt(0);
+        if (byteNumber != message.substring(1).length() + 1){
+            System.out.println("The received frame has an error!");
+            return;
+        }
+        System.out.println(message.substring(1));
     }
 }
--- project1_framing/src/Sender.java
+++ project1_framing/src/Sender.java
@@ -1,19 +1,45 @@
+import java.util.Scanner;
+
 public class Sender {
 
     public static void main (String[] args){
-        
+        System.out.println("Enter which framing method you would like to use: ");
+        System.out.println("Choose: Bit Stuffing, Byte Stuffing, or Byte Count");
+//       byteCount();
+        bitStuffing();
+
     }
 
-    public static String bitStuffing(){
-        return "";
+    public static void bitStuffing(){
+
+        System.out.println("Enter a message as s sender to be prepared in the format of a valid receiver: ");
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        String flag = "111111";
+        int oneCount = 0;
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) == '1'){
+                oneCount++;
+                if(oneCount == 6){
+                    message = message.substring(0, i) + "0" + message.substring(i, message.length());
+                    oneCount = 0;
+                }
+            }
+        }
+        message = flag+message+flag;
+        System.out.println(message);
     }
 
-    public static String byteStuffing(){
-        return "";
+    public static void byteStuffing(){
     }
 
-    public static String byteCount(){
-        return "";
+    public static void byteCount(){
+        System.out.println("Enter a message as s sender to be prepared in the format of a valid receiver: ");
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        String sentMessage = String.valueOf(message.length() + 1);
+        message = sentMessage + message;
+        System.out.println(message);
     }
 
 }
