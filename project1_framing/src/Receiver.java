5b26ab1c31914ff303a64f673b466109f0683107
Michael Noonan <manoonan@bsu.edu>
2018-02-08 23:29:40 -0500

    Completed Project

--- project1_framing/src/Receiver.java
+++ project1_framing/src/Receiver.java
@@ -5,9 +5,20 @@ public class Receiver {
     public static void main (String[] args){
         System.out.println("Enter which framing method you would like to use: ");
         System.out.println("Choose: Bit Stuffing, Byte Stuffing, or Byte Count");
-//        byteCount();
-//        byteStuffing();
-        bitStuffing();
+        Scanner in = new Scanner(System.in);
+        String choice = in.nextLine();
+        switch (choice){
+            case "Bit Stuffing": bitStuffing();
+                break;
+
+            case "Byte Stuffing": byteStuffing();
+                break;
+
+            case "Byte Count": byteCount();
+                break;
+
+            default: System.out.println("Not a valid framing method.");
+        }
 
     }
 
@@ -17,40 +28,52 @@ public class Receiver {
         String message = in.nextLine();
         String flag = "111111";
         int oneCount = 0;
-        message = message.replaceAll(flag, "");
-        for (int i = 0; i < message.length(); i++) {
-            if (message.charAt(i) == '1'){
-                oneCount++;
-                if(oneCount == 5){
-                    message = message.substring(0, i) + "" + message.substring(i, message.length());
-                    oneCount = 0;
+        if (flag.equals(message.substring(0,6)) && flag.equals(message.substring(message.length() - 6, message.length()))){
+            message = message.substring(6, message.length() - 6);
+            System.out.println("The message without flag bytes = " + message);
+            for (int i = 0; i < message.length(); i++) {
+                if (message.charAt(i) == '1'){
+                    oneCount++;
+                    if (oneCount == 5){
+                        if (message.charAt(i + 1) == '0'){
+                            message = message.substring(0, i + 1) + message.substring( i + 1 + 1);
+                            oneCount = 0;
+                        }
+                    }
+                    if (oneCount == 6){
+                        System.out.println("The received frame has an error!");
+                        return;
+                    }
                 }
             }
+            System.out.println("The message without flag bytes and stuffed 0 bits = " + message);
         }
-        System.out.println(message);
+        else {
+            System.out.println("The received frame has an error!");
+        }
     }
 
     public static void byteStuffing(){
         System.out.println("Enter a message to the receiver in the format of a valid sender: ");
         Scanner in = new Scanner(System.in);
         String message = in.nextLine();
+        if (!(message.contains("E") || message.contains("F") || message.contains("e") || message.contains("f"))){
+            System.out.println("The received frame has an error!");
+            return;
+        }
         message = message.replaceAll("[EFef]", "");
-//        if (message.contains("E") || message.contains("F")){
-//            System.out.println("The received frame has an error!");
-//            return;
-//        }
-        System.out.println(message);
+        System.out.println("The message sent without flag bytes = " + message);
     }
 
     public static void byteCount(){
         System.out.println("Enter a message to the receiver in the format of a valid sender: ");
         Scanner in = new Scanner(System.in);
         String message = in.nextLine();
-        int byteNumber = (int) message.charAt(0);
-        if (Integer.valueOf(byteNumber) != message.substring(1).length() + 1){
+        Integer byteNumber = Character.getNumericValue(message.charAt(0));
+        if (byteNumber != message.substring(1).length() + 1){
             System.out.println("The received frame has an error!");
             return;
         }
-        System.out.println(message.substring(1));
+        System.out.println("The message = " + message.substring(1));
     }
 }
--- project1_framing/src/Sender.java
+++ project1_framing/src/Sender.java
@@ -5,18 +5,33 @@ public class Sender {
     public static void main (String[] args){
         System.out.println("Enter which framing method you would like to use: ");
         System.out.println("Choose: Bit Stuffing, Byte Stuffing, or Byte Count");
-//       byteCount();
-        bitStuffing();
+        Scanner in = new Scanner(System.in);
+        String choice = in.nextLine();
+        switch (choice){
+            case "Bit Stuffing": bitStuffing();
+            break;
 
+            case "Byte Stuffing": byteStuffing();
+            break;
+
+            case "Byte Count": byteCount();
+            break;
+
+            default: System.out.println("Not a valid framing method.");
+        }
     }
 
     public static void bitStuffing(){
 
-        System.out.println("Enter a message as s sender to be prepared in the format of a valid receiver: ");
+        System.out.println("Enter a message as a sender to be prepared in the format of a valid receiver: ");
         Scanner in = new Scanner(System.in);
         String message = in.nextLine();
         String flag = "111111";
         int oneCount = 0;
+        if (!(message.contains("1") || message.contains("0"))){
+            System.out.println("The received frame has an error!");
+            return;
+        }
         for (int i = 0; i < message.length(); i++) {
             if (message.charAt(i) == '1'){
                 oneCount++;
@@ -27,19 +42,25 @@ public class Sender {
             }
         }
         message = flag+message+flag;
-        System.out.println(message);
+        System.out.println("The message will be sent to the receiver in this format: " + message);
     }
 
     public static void byteStuffing(){
+        System.out.println("Enter a message as a sender to be prepared in the format of a valid receiver: ");
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        String flag = "EF";
+        message = flag+message+flag;
+        System.out.println("The message will be sent to the receiver in this format: " + message);
     }
 
     public static void byteCount(){
-        System.out.println("Enter a message as s sender to be prepared in the format of a valid receiver: ");
+        System.out.println("Enter a message as a sender to be prepared in the format of a valid receiver: ");
         Scanner in = new Scanner(System.in);
         String message = in.nextLine();
         String sentMessage = String.valueOf(message.length() + 1);
         message = sentMessage + message;
-        System.out.println(message);
+        System.out.println("The message will be sent to the receiver in this format: " + message);
     }
 
 }
